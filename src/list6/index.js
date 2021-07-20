812a91e6654eb2a74fc330e5fdb15df38655fb48
hermanke <hermanke@tencent.com>
2021-07-20 11:59:02 +0800

    test @babel/preset-env

--- src/list6/index.js
+++ src/list6/index.js
@@ -1,9 +1,15 @@
+// import "@babel/polyfill";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import _ from 'lodash';
+// import _ from 'lodash';
+
+import {promise} from './promise';
 
 function App() {
   const text = _.add(99, 1);
+  promise.then(()=>{
+    console.log(456);
+  })
   return (
     <div>
       <div>list page</div>
---
+++ src/list6/promise.js
@@ -0,0 +1,3 @@
+export const promise = new Promise((resolve) => {
+  resolve(123);
+});
